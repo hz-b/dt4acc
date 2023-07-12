[TASK] debugging bba: improving interface records, muxer event progression beam: exporting fixed point, if found, calculation times muxer: only progress values for the ones that changed

python/calculator.py
@@ -8,7 +8,7 @@ from thor_scsi.utils.accelerator import (
 import gtpsa
 
 # there are some memory management problems in thor scsi
-desc = gtpsa.desc(6, 2)
+desc = gtpsa.desc(6, 1)
 
 logger = logging.getLogger("thor-scsi-lib")
 
@@ -23,9 +23,11 @@ class Calculator:
 
 class TwissCalculator(Calculator):
     def calculate(self):
+        logger.warning("Computing Twiss parameters (start)")
         n_dof = 2
         self.result = compute_Twiss_along_lattice(n_dof, self.parent.acc, self.parent.conf, desc=desc,
                                                   mapping=self.parent.mapping)
+        logger.warning("Computed  Twiss parameters (end)  ")
         return self.result
 
 
@@ -48,15 +50,15 @@ class OrbitCalculator(Calculator):
 
         self.eps = eps
 
-        self.eps = 1e-15
-        logger.warning(f"got {eps=} forced to {self.eps} ")
+        # self.eps = 1e-15
+        # logger.warning(f"got {eps=} forced to {self.eps} ")
 
     def get_epsilon(self):
         return self.eps
 
     def calculate(self, *, mapping, desc=None):
         # Determine whether delta and x0 need to be set
-        logger.info(f"Before calculating: last stored result {self.result=} eps = {self.eps}")
+        logger.debug("Before calculating: last stored result %s eps = %s", self.result, self.eps)
         if self.result is None or not np.isfinite(self.result.x0.iloc).all():
             delta = 0.0
             x0 = None
@@ -64,7 +66,13 @@ class OrbitCalculator(Calculator):
             delta = None
             x0 = self.result.x0
 
-        # start alw
+        # start with last settings
+        px0 = None
+        if x0 is not None:
+            px0 = x0 * 1000
+        logger.warning("Calculating orbit with x0 * 1000 = %s for eps %s", px0, self.eps)
+        del px0
+
         try:
             # Call compute_closed_orbit with the appropriate arguments
             self.result = compute_closed_orbit(
@@ -88,6 +96,6 @@ class OrbitCalculator(Calculator):
 
         # Calculate the orbit using the updated result object
         self.orbit = extract_orbit_from_accelerator_with_standard_observers(self.parent.acc)
-        logger.warning(f"Calculated Orbit x0 * 1000 = {self.result.x0 * 1000} for eps {self.eps}")
+        logger.warning(f"Calculated Orbit x0 * 1000 = %s for eps %s", self.result.x0 * 1000, self.eps)
 
         return self.result

python/virtual_accelerator.py
@@ -8,7 +8,6 @@ from .pending_calculation_manager import PendingCalculationManager
 from .calculation_running_manager import CalculationRunningManager
 from .delay_execution import DelayExecution
 from .bpm_mimikry import BPMMimikry
-import logging
 from typing import Sequence
 from thor_scsi.utils.extract_info import accelerator_info
 
@@ -159,7 +158,14 @@ class VirtualAccelerator:
 
         logger.warning("Executing twiss calculation")
         with self.twiss_pending:
+            tic = time.time()
             self._calculate_twiss()
+            tac = time.time()
+            dt = tac - tic
+
+            label = f"{self.prefix}:beam:twiss:calc_time"
+            logger.info("Executing pydev.iontr(%s, %s)", label, dt)
+            pydev.iointr(label, dt)
 
         logger.warning(f"Twiss: after calculation still need? {self.twiss_pending.pending}")
 
@@ -170,7 +176,14 @@ class VirtualAccelerator:
 
         logger.warning("Executing orbit calculation")
         with self.orbit_pending:
+            tic = time.time()
             r = self._calculate_orbit()
+            tac = time.time()
+            dt = tac - tic
+
+            label = f"{self.prefix}:beam:orbit:calc_time"
+            logger.info("Executing pydev.iontr(%s, %s)", label, dt)
+            pydev.iointr(label, dt)
 
         logger.warning("Orbit: after calculation still need? %d", self.orbit_pending.pending)
 
@@ -210,19 +223,34 @@ class VirtualAccelerator:
         # Calculate the orbit
         r = self.accelerator_facade.calculate_orbit()
 
+        prefix = self.prefix + ":beam"
+
+        # export closed orbit ...
+        # found, fixed point, orbit
+        # orbit only if found
+        label = f"{prefix}:orbit:found"
+        logger.info(f"Executing pydev.iontr({label}, {r.found_closed_orbit})")
+        pydev.iointr(label, r.found_closed_orbit)
+
+        label = f"{prefix}:orbit:fixed_point"
+        # towards mm / mrad
+        x0 = np.array(r.x0.iloc) * 1000.0
+        x0 = list(x0)
+        logger.info(f"Executing pydev.iontr({label}, {x0}")
+        pydev.iointr(label, list(x0))
+
         # Raise a ValueError if no closed orbit is found
         if not r.found_closed_orbit:
             raise ValueError("No closed orbit was found")
 
         # Get the orbit data
         ds = self.accelerator_facade.orbit.orbit
-        prefix = self.prefix + ":beam"
 
         # Publish orbit data for x and y planes
         for plane in "x", "y":
             val = ds.ps.sel(phase_coordinate=plane).values.tolist()
             label = f"{prefix}:orbit:{plane}"
-            logger.info(f"Executing pydev.iontr({label}, {val[:3]} {type(val)}")
+            logger.info(f"Executing pydev.iontr({label}, {val[:3]} {type(val)})")
             pydev.iointr(label, val)
 
         # Publish BPM data