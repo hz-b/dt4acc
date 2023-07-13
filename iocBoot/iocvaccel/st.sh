146cabd530f5415bdc5aa72277a82fcf49d10fb6
Waheed <wsulimankhail@gmail.com>
2023-07-13 15:12:39 +0200

    [tASK] library path updated

--- iocBoot/iocvaccel/st.sh
+++ iocBoot/iocvaccel/st.sh
@@ -3,12 +3,12 @@
 
 # just to make sure
 T_DIR=`pwd`
-
+L_DIR=$HOME/.local/
 echo "Current directory: ${T_DIR}"
 
 cd ../../
 
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/lib:"$L_DIR"/lib:$LD_LIBRARY_PATH
 
 cd ${T_DIR}
 
