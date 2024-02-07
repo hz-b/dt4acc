90d24c1679f63fb7f0edf55ebc8e5ab1144b267f
Waheed <wsulimankhail@gmail.com>
2024-02-07 11:10:06 +0100

    [TASK] accelerators are separated

--- src/tests/test_nvacc.py
+++ src/tests/test_nvacc.py
@@ -1,8 +1,13 @@
-import pytest
-import pydev
-
 from src.dt4acc.command import update
 
 
 def test_update_simple():
-    update(element_id='q1m1d1r', property_name='dx', value=0.5e-3)
+    update(element_id='Q1M1D1R', property_name='dx', value=0.5e-3)
+
+
+def test_update_quad_strength():
+    update(element_id='Q1M1D1R', property_name='main_multipole_strength', value=1.2)
+
+
+def test_update_horizontal_steerer():
+    update(element_id='Q1M1D1R', property_name='main_multipole_strength', value=1.2)
