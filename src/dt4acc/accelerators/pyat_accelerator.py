3d3c76b793bb8a34070fe3c1cfbd556b3704c968
Waheed <wsulimankhail@gmail.com>
2024-04-30 17:20:40 +0200

    lat2db accelerator is used as asselerator for dt

--- src/dt4acc/accelerators/pyat_accelerator.py
+++ src/dt4acc/accelerators/pyat_accelerator.py
@@ -6,39 +6,13 @@ from ..accelerators.proxy_factory import PyATProxyFactory
 from ..calculator.pyat_calculator import PyAtTwissCalculator, PyAtOrbitCalculator
 from ..device_interface.bpm_mimikry import BPMMimikry
 from ..model.orbit import Orbit
-from ..resources.bessy2_sr_reflat import bessy2Lattice
 from ..view.calculation_result_view import ResultView, ElementParameterView
-
 calculation_lock = threading.Lock()
 
 
 def set_pyat_ring():
-    import at
-    from lat2db.tools.factories.pyat import factory
-    from pymongo import MongoClient
-
-    return bessy2Lattice()
-    # get database
-    client = MongoClient("mongodb://127.0.0.1:27017/")
-    db = client["bessyii"]
-    collection = db["machines"]
-    lattice_in_json_format = collection.find_one()
-    # if there is no record in db, this means the lat2db project is not yet used.
-    # todo: if someone does not have mongo installed at all?
-    #   * maybe he should install and start mongo before getting to this point
-    #   * remove the below two lines
-    if lattice_in_json_format is None:
-        return bessy2Lattice()
-    seq = factory(lattice_in_json_format)
-
-    ring = at.Lattice(seq, name='bessy2', periodicity=1, energy=1.7e9)
-    if True:
-        # set up of calculation choice
-        ring.enable_6d()  # Should 6D be default?
-        # Set main cavity phases
-        ring.set_cavity_phase(cavpts='CAV*')
-    return ring
-
+    from lat2db.model.accelerator import Accelerator
+    return Accelerator().ring
 
 acc = set_pyat_ring()
 prefix = "Pierre:DT"
