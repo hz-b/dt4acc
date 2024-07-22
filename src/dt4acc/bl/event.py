8f2605ee9bef8e0be07cb04cbdeffd1aebfc3b0c
Waheed <wsulimankhail@gmail.com>
2024-07-22 13:26:55 +0200

    event back to as it was

--- src/dt4acc/bl/event.py
+++ src/dt4acc/bl/event.py
@@ -6,26 +6,19 @@ class Event:
         self.callbacks = []
         self.event_queue = asyncio.Queue()
 
-    async def subscribe(self, callback):
-        status = self.callbacks.append(callback)
-        if status is None:
-            print(callback)
-        await status
+    def subscribe(self, callback):
+        self.callbacks.append(callback)
 
     async def trigger(self, obj):
         # Process each callback asynchronously but in sequence
-        if len(self.callbacks) == 0:
-            print(self)
-            return
         for callback in self.callbacks:
             await callback(obj)  # Ensure your callbacks are designed as coroutine functions
-
 
     async def process_events(self):
         while True:
             obj, callbacks = await self.event_queue.get()
             for callback in callbacks:
-                callback(obj)  # Ensure your callbacks are coroutine functions
+                await callback(obj)  # Ensure your callbacks are coroutine functions
 
 
 class StatusChange(Event):
