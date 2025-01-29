18f076aa014783b744c1621d0e4ce8019a5f4a12
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-01-29 21:44:44 +0100

    [TASK] handling nearly all devices directly ...

--- src/dt4acc/core/command.py
+++ src/dt4acc/core/command.py
@@ -4,6 +4,7 @@ from bact_twin_bessyii_impl.bl.io.pytac_repositories import PyTACRepository
 from bact_twin_bessyii_impl.bl.command_rewritter import CommandRewriter
 from bact_twin_bessyii_impl.bl.translation_service import TranslationService
 
+from .accelerators.accelerator_manager import AcceleratorManager
 from .accelerators.pyat_accelerator import setup_accelerator
 from .update_context_manager import UpdateContext
 
@@ -14,8 +15,9 @@ class UpdateManager:
     Treats the incoming requests as commands to be rewritten and delivered to the
     machine
     """
-    def __init__(self, command_rewritter: CommandRewriterBase):
+    def __init__(self, command_rewritter: CommandRewriterBase, acc_mgr: AcceleratorManager):
         self.command_rewritter = command_rewritter
+        self.acc_mgr = acc_mgr
 
     async def update(self, *, device_id, property_name, value=None, element=None):
         """Update an device property using element knowledge
@@ -40,23 +42,5 @@ class UpdateManager:
                 #
                 # Todo: revisit if a transactional update should be applied here
 
-                elem_proxy = await acc.accelerator.get_element(cmd.id)
+                elem_proxy = await self.acc_mgr.accelerator.get_element(cmd.id)
                 await elem_proxy.update(cmd.property, cmd.value, element)
-
-
-#: Todo should be in the main startup script
-acc = setup_accelerator()
-
-#: todo replace soon by database service
-repo = PyTACRepository()
-
-
-#: should be in main startup script
-update_manager = UpdateManager(
-    command_rewritter=CommandRewriter(
-        TranslationService(conversion_info=repo.state_conversion_repo)
-    )
-)
-
-async def update(*, device_id, property_name, value=None, element=None):
-    return await update_manager.update(device_id=device_id, property_name=property_name, value=value, element=element)
--- src/dt4acc/custom_epics/ioc/handlers.py
+++ src/dt4acc/custom_epics/ioc/handlers.py
@@ -1,11 +1,26 @@
+from bact_twin_bessyii_impl.bl.command_rewritter import CommandRewriter
+from bact_twin_bessyii_impl.bl.io.pytac_repositories import PyTACRepository
+from bact_twin_bessyii_impl.bl.translation_service import TranslationService
 from p4p.client.asyncio import Context
 
-from ...core import command
+from ...core.accelerators.pyat_accelerator import setup_accelerator
+from ...core.command import UpdateManager
 from ...core.utils.logger import get_logger
 
 logger = get_logger()
 
 ctx = Context("pva")  # Create a context for EPICS PVA (PV Access)
+
+#: todo replace soon by database service
+repo = PyTACRepository()
+
+# todo: should this be part of the controller
+update_manager = UpdateManager(
+    command_rewritter=CommandRewriter(
+        TranslationService(conversion_info=repo.state_conversion_repo)
+    ),
+    acc_mgr=setup_accelerator()
+)
 
 
 async def handle_device_update(device_id: str, property_id: str, value: float):
@@ -26,93 +41,6 @@ async def handle_device_update(device_id: str, property_id: str, value: float):
     """
     try:
         # Call the command update function to apply the value to the model
-        await command.update(device_id=device_id, property_name=property_id, value=value)
+        await update_manager.update(device_id=device_id, property_name=property_id, value=value)
     except Exception as e:
         logger.warning(f"Error in updating element {device_id} with property_name: {property_id} value {value}")
-
-
-async def handle_magnet_update(device_id: str, property_id: str, value: float):
-    """
-    Handles updates to magnet PVs.
-
-    Args:
-        device_name (str): The process variable (PV) name of the magnet.
-        value (float): The updated value for the PV.
-        element (object): The magnet element being updated.
-
-    Updates the corresponding magnet property in the accelerator model.
-    """
-    try:
-        # Call the command update function to apply the value to the model
-        await command.update(device_id=device_id, property_name=property_id, value=value)
-    except Exception as e:
-        logger.warning(f"Error in updating {device_id=} {property_id=} {value=}: {e}")
-
-
-
-async def handle_power_converter_update(pc_name, value, prefix, connected_magnets):
-    """
-    Handles updates to power converters and propagates changes to connected magnets.
-
-    Args:
-        pc_name (str): The name of the power converter.
-        value (float): The updated value for the power converter.
-        prefix (str): The prefix used for naming PVs.
-        connected_magnets (list): List of magnets connected to this power converter.
-
-    Updates the power converter readback value and the connected magnets.
-    """
-    try:
-        await ctx.put(f"{prefix}:{pc_name}:rdbk", value)
-    except Exception as e:
-        logger.warning(f"Error updating power converter rdbk {pc_name}: {e}")
-    # Update the `im:I` PVs of all connected magnets
-    try:
-        for magnet in connected_magnets:
-            await ctx.put(f"{prefix}:{magnet['name']}:im:I", value)
-    except Exception as e:
-        logger.warning(f"Error updating power converter {pc_name}: {e}")
-
-
-async def handle_master_clock_update(cavity_names, value, prefix):
-    """
-    Handles updates to the master clock and updates frequency PVs for cavities.
-
-    Args:
-        cavity_names (list): List of cavity PV names to update.
-        value (float): The updated frequency value.
-        prefix (str): The prefix used for naming PVs.
-
-    Updates the frequency of all cavities and applies it to the accelerator model.
-    """
-    for cavity in cavity_names:
-        try:
-            await ctx.put(f"{prefix}:{cavity}:freq", value)
-            await command.update(element_id=cavity, property_name='freq', value=value, element=cavity_names)
-        except Exception as e:
-            logger.info(f"Error updating cavity frequency {cavity}: {e}")
-
-
-def get_property_id(pv_name):
-    """
-    todo: this is ugly at the moment
-    Determines the property ID based on the provided PV name.
-
-    Args:
-        pv_name (str): The process variable (PV) name.
-
-    Returns:
-        str: The corresponding property ID, or None if not recognized.
-    """
-    if ':Cm:set' in pv_name:
-        return 'K'
-    elif ':x:set' in pv_name:
-        return 'x'
-    elif ':y:set' in pv_name:
-        return 'y'
-    elif ':im:I' in pv_name:
-        return 'im'
-    elif ':rdbk' in pv_name:
-        return 'rdbk'
-    else:
-        return None
