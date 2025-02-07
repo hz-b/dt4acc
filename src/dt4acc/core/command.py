5ee1b070299fd5dad31e8771b76a774b83c88c1f
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-02-07 16:26:10 +0100

    [TASK] added cavity, power converters, frequency

--- src/dt4acc/core/command.py
+++ src/dt4acc/core/command.py
@@ -1,8 +1,18 @@
+from typing import Sequence
+
 from bact_twin_architecture.data_model.command import Command, BehaviourOnError
+from bact_twin_architecture.data_model.identifiers import (
+    LatticeElementPropertyID,
+    DevicePropertyID,
+    ConversionID,
+)
 from bact_twin_architecture.interfaces.command_rewritter import CommandRewriterBase
+from bact_twin_architecture.interfaces.liaison_manager import LiaisonManagerBase
+from bact_twin_architecture.interfaces.translator_service import TranslatorServiceBase
 
 from .accelerators.accelerator_manager import AcceleratorManager
 from .update_context_manager import UpdateContext
+from ..custom_epics.ioc.liasion_translation_manager import TranslatorService
 
 
 class UpdateManager:
@@ -10,17 +20,59 @@ class UpdateManager:
 
     Treats the incoming requests as commands to be rewritten and delivered to the
     machine
+
+    Supports peeking into the engine
     """
-    def __init__(self, command_rewritter: CommandRewriterBase, acc_mgr: AcceleratorManager):
+
+    def __init__(
+        self,
+        *,
+        command_rewritter: CommandRewriterBase,
+        liaison_manager: LiaisonManagerBase,
+        translator_service: TranslatorServiceBase,
+        acc_mgr: AcceleratorManager,
+    ):
         self.command_rewritter = command_rewritter
+        self.liaison_manager = liaison_manager
+        self.translator_service = translator_service
         self.acc_mgr = acc_mgr
 
-    def peek(self, lat_elem, property_name) -> object:
+    def device_value_from_peeking_engine(
+        self, dev_prop: DevicePropertyID
+    ) -> Sequence[float]:
         """
-        Todo: remove layer violation
+        Todo:
+            review interface
+            place it in correct layer
+
+            How to handle that many values can be returned
         """
-        proxy = self.acc_mgr.accelerator.proxy_factory.get(lat_elem)
-        val = proxy.peek(property_id=property_name)
+        if dev_prop.device_name[:3].upper() == "CAV":
+            pass
+        lat_props = self.liaison_manager.inverse(dev_prop)
+        if lat_props is None:
+            raise AssertionError(
+                f"{self.liaison_manager.__class__.__name__} does not know {dev_prop}"
+            )
+
+        def convert(lat_prop):
+            translator = self.translator_service.get(
+                ConversionID(lattice_property_id=lat_prop, device_property_id=dev_prop)
+            )
+            val = self.peek_engine(lat_prop)
+            return translator.forward(val)
+
+        values = [convert(lat_prop) for lat_prop in lat_props]
+        return values
+
+    def peek_engine(self, lat_elem_prop: LatticeElementPropertyID) -> object:
+        """peek into underlaying engine to get value
+
+        Todo:
+            resolve layring violation
+        """
+        proxy = self.acc_mgr.accelerator.proxy_factory.get(lat_elem_prop.element_name)
+        val = proxy.peek(property_id=lat_elem_prop.property)
         return val
 
     async def update(self, *, device_id, property_name, value=None, element=None):
@@ -34,11 +86,21 @@ class UpdateManager:
         # this argument shall be removed
         assert element is None
         # update context manager: currently here as the async io comm stops at first exception
-        with UpdateContext(element_id=device_id, property_name=property_name, value=value, element=element,
-                           kwargs=dict()):
+        with UpdateContext(
+            element_id=device_id,
+            property_name=property_name,
+            value=value,
+            element=element,
+            kwargs=dict(),
+        ):
             cmds = self.command_rewritter.inverse(
-                Command(id=device_id, property=property_name, value=value, behaviour_on_error=BehaviourOnError.stop
-            ))
+                Command(
+                    id=device_id,
+                    property=property_name,
+                    value=value,
+                    behaviour_on_error=BehaviourOnError.stop,
+                )
+            )
             cmds
             for cmd in cmds:
                 # Todo: simplify the code down here ...
--- src/dt4acc/custom_epics/ioc/pv_setup.py
+++ src/dt4acc/custom_epics/ioc/pv_setup.py
@@ -1,13 +1,20 @@
-import logging
-
 import numpy as np
+from bact_twin_architecture.data_model.identifiers import (
+    LatticeElementPropertyID,
+    DevicePropertyID,
+)
+from p4p.asLib.yacc import start
 
 from .handlers import handle_device_update, update_manager
 from ..data.constants import config, special_pvs, cavity_names
-from ..data.querries import get_unique_power_converters, get_magnets_per_power_converters
+from ..data.querries import (
+    get_unique_power_converters,
+    get_magnets_per_power_converters,
+)
 from ...core.utils.logger import get_logger
 
 logger = get_logger()
+
 
 def flag_not_handling(pv_name: str, val: object):
     logger.warning("Not handling update of pv %s to %s", pv_name, val)
@@ -27,22 +34,37 @@ def initialize_magnet_pvs(builder, magnet):
         - `<magnet_name>:x:set`: Horizontal position setpoint
         - `<magnet_name>:y:set`: Vertical position setpoint
     """
-    magnet_name = magnet['name']
+    magnet_name = magnet["name"]
     # Create an element representing the magnet
     # Create PVs and link to update logic
-    val = update_manager.peek(magnet_name, "main_strength")
-    builder.aOut(f"{magnet_name}:Cm:set", initial_value=magnet["k"] or 0,
-                 on_update=lambda val: handle_device_update(magnet_name, "K", val))
+    val = update_manager.peek_engine(
+        LatticeElementPropertyID(element_name=magnet_name, property="main_strength")
+    )
+    builder.aOut(
+        f"{magnet_name}:Cm:set",
+        initial_value=magnet["k"] or 0,
+        on_update=lambda val: handle_device_update(magnet_name, "K", val),
+    )
     builder.aIn(f"{magnet_name}:Cm:rdbk", initial_value=val)
-    builder.aOut(f"{magnet_name}:im:I", initial_value=0.0,
-                 # Todo: what to do if current is set, should be rather read only
-                 # on_update=lambda val: handle_device_update(f"{magnet_name}:im:I", val)
-                 on_update=lambda val: handle_device_update(magnet_name, "powersupply_current", val)
-                 )
-    builder.aOut(f"{magnet_name}:x:set", initial_value=0.0,
-                 on_update=lambda val: handle_device_update(magnet_name, "x", val))
-    builder.aOut(f"{magnet_name}:y:set", initial_value=0.0,
-                 on_update=lambda val: handle_device_update(magnet_name, "y", val))
+    builder.aOut(
+        f"{magnet_name}:im:I",
+        initial_value=0.0,
+        # Todo: what to do if current is set, should be rather read only
+        # on_update=lambda val: handle_device_update(f"{magnet_name}:im:I", val)
+        on_update=lambda val: handle_device_update(
+            magnet_name, "powersupply_current", val
+        ),
+    )
+    builder.aOut(
+        f"{magnet_name}:x:set",
+        initial_value=0.0,
+        on_update=lambda val: handle_device_update(magnet_name, "x", val),
+    )
+    builder.aOut(
+        f"{magnet_name}:y:set",
+        initial_value=0.0,
+        on_update=lambda val: handle_device_update(magnet_name, "y", val),
+    )
 
 
 def initialize_power_converter_pvs(builder, prefix):
@@ -67,7 +89,7 @@ def add_pc_pvs(builder, pc_name, prefix):
         prefix (str): Prefix for PVs.
     """
     magnets = get_magnets_per_power_converters(pc_name)
-    element = {'magnets': [item['name'] for item in magnets]}
+    element = {"magnets": [item["name"] for item in magnets]}
     element_cache = {}  # Store magnet information for reference
     element_cache[pc_name] = element
 
@@ -77,10 +99,17 @@ def add_pc_pvs(builder, pc_name, prefix):
 
     # Create power converter setpoint and readback PVs
     # Todo: put it to power converters directly
-    builder.aOut(f"{pc_name}:set", initial_value=0.0,
-                 on_update=lambda val: handle_device_update(pc_name, "set_current", val)
+    vals = update_manager.device_value_from_peeking_engine(
+        DevicePropertyID(device_name=pc_name, property="set_current")
     )
-    builder.aOut(f"{pc_name}:rdbk", initial_value=0.0)
+    start_val = np.asarray(vals).mean()
+    builder.aOut(
+        f"{pc_name}:set",
+        initial_value=start_val,
+        on_update=lambda val: handle_device_update(pc_name, "set_current", val),
+    )
+    #: todo ensur that readback is updated
+    builder.aOut(f"{pc_name}:rdbk", initial_value=start_val)
 
 
 def initialize_orbit_pvs(builder):
@@ -93,7 +122,9 @@ def initialize_orbit_pvs(builder):
     builder.WaveformOut(f"beam:orbit:x", initial_value=[0.0], length=config.n_elements)
     builder.WaveformOut(f"beam:orbit:y", initial_value=[0.0], length=config.n_elements)
     builder.WaveformOut(f"beam:orbit:x0", initial_value=[0.0], length=config.n_elements)
-    builder.WaveformOut(f"beam:orbit:names", initial_value=[""], length=config.n_elements)
+    builder.WaveformOut(
+        f"beam:orbit:names", initial_value=[""], length=config.n_elements
+    )
     builder.aOut(f"beam:orbit:found", initial_value=0)
 
 
@@ -104,23 +135,61 @@ def initialize_twiss_pvs(builder):
     Args:
         builder: The SoftIOC PV builder instance.
     """
-    for axis in ['x', 'y']:
-        builder.WaveformOut(f"beam:twiss:{axis}:alpha", initial_value=[0.0], length=config.n_elements)
-        builder.WaveformOut(f"beam:twiss:{axis}:beta", initial_value=[0.0], length=config.n_elements)
-        builder.WaveformOut(f"beam:twiss:{axis}:nu", initial_value=[0.0], length=config.n_elements)
-    builder.WaveformOut(f"beam:twiss:names", initial_value=[""], length=config.n_elements)
+    for axis in ["x", "y"]:
+        builder.WaveformOut(
+            f"beam:twiss:{axis}:alpha", initial_value=[0.0], length=config.n_elements
+        )
+        builder.WaveformOut(
+            f"beam:twiss:{axis}:beta", initial_value=[0.0], length=config.n_elements
+        )
+        builder.WaveformOut(
+            f"beam:twiss:{axis}:nu", initial_value=[0.0], length=config.n_elements
+        )
+    builder.WaveformOut(
+        f"beam:twiss:names", initial_value=[""], length=config.n_elements
+    )
+
+
+def initialize_master_clock_pvs(builder):
+    """initalise master clock pv
+
+    Warning:
+        note for running the twin as a shadow it will
+        require precise frequency tuning
+
+    Todo:
+        Foresee dedicated variables for allowing only a difference shift
+        Provide the frequency the code starts with
+    """
+    vals = update_manager.device_value_from_peeking_engine(
+        DevicePropertyID(device_name="master_clock", property="reference_frequency")
+    )
+    start_val = np.asarray(vals).mean()
+    builder.aOut(
+        f"{special_pvs['master_clock']}:freq",
+        initial_value=start_val,
+        always_update=True,
+        EGU="kHz",
+        PREC=3,
+        on_update=lambda val: handle_device_update(
+            device_id="master_clock", property_id="reference_frequency", value=val
+        ),
+    )
+    builder.aIn("lattice_info:ref_freq", initial_value=start_val, EGU="kHz", PREC=1)
+    builder.longIn(
+        "lattice_info:ref_freq:khz:up", initial_value=int(start_val), EGU="kHz"
+    )
+    frac = (start_val % 1) * 1e6
+    builder.longIn("lattice_info:ref_freq:khz:frac", initial_value=int(frac), EGU="mHz")
 
 
 def initialize_other_pvs(builder, prefix):
-    """
-    Initializes miscellaneous PVs including master clock and dummy values.
+    """Initializes miscellaneous PVs (dummy values).
 
     Args:
         builder: The SoftIOC PV builder instance.
         prefix (str): Prefix for PV naming.
     """
-    builder.aOut(f"{special_pvs['master_clock']}:freq", initial_value=0,
-                 on_update=lambda val: handle_device_update(device_id="master_clock", property_id="reference_frequency", value=val))
     builder.aOut(f"dummy:x", initial_value=0)
     builder.aOut(f"dummy:y", initial_value=0)
     builder.aOut(f"{special_pvs['current']}:current", initial_value=0)
@@ -128,9 +197,11 @@ def initialize_other_pvs(builder, prefix):
 
 def initialize_bpm_pvs(builder):
     tmp = np.empty([2048], np.int16)
-    tmp.fill(-2 ** 15 + 1)
+    tmp.fill(-(2 ** 15) + 1)
     # bpm pv names from default
-    builder.WaveformOut(f"{special_pvs['bpm_pv']}:bdata", initial_value=tmp, length=len(tmp))
+    builder.WaveformOut(
+        f"{special_pvs['bpm_pv']}:bdata", initial_value=tmp, length=len(tmp)
+    )
     builder.longOut(f"{special_pvs['bpm_pv']}:count", initial_value=0)
 
 
@@ -140,6 +211,14 @@ def initialize_cavity_pvs(builder):
 
     Args:
         builder: The SoftIOC PV builder instance.
+
+    Todo:
+        check that these are updated if the master clock changes
     """
+
     for cavity_name in cavity_names:
-        builder.aOut(f"{cavity_name}:freq", initial_value=0.0)
+        vals = update_manager.device_value_from_peeking_engine(
+            DevicePropertyID(device_name=cavity_name, property="frequency")
+        )
+        start_val = np.asarray(vals).mean()
+        builder.aOut(f"{cavity_name}:freq", initial_value=start_val, EGU="kHz", PREC=3)
