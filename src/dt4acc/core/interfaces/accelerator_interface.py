687437a9cd58945bca8d73a6309ea61c6112b756
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-02-07 16:26:10 +0100

    [TASK] added cavity, power converters, frequency

--- src/dt4acc/core/interfaces/accelerator_interface.py
+++ src/dt4acc/core/interfaces/accelerator_interface.py
@@ -9,11 +9,14 @@ class AcceleratorInterface(metaclass=ABCMeta):
     Todo:
         Derive from a list interface
     """
+
     @abstractmethod
     def get_element(self, element_id) -> ElementInterface:
+        """
+        Review if derived classes use async implementations
+        """
         pass
 
     @abstractmethod
     def __init__(self, *args, **kwargs):
         pass
-
--- src/dt4acc/custom_epics/ioc/liasion_translation_manager.py
+++ src/dt4acc/custom_epics/ioc/liasion_translation_manager.py
@@ -1,16 +1,30 @@
 from typing import Dict, Sequence, Mapping
 import logging
 
-from bact_twin_architecture.data_model.identifiers import LatticeElementPropertyID, DevicePropertyID, ConversionID
+from bact_twin_architecture.data_model.identifiers import (
+    LatticeElementPropertyID,
+    DevicePropertyID,
+    ConversionID,
+)
 from bact_twin_architecture.interfaces.liaison_manager import LiaisonManagerBase
 from bact_twin_architecture.interfaces.state_conversion import StateConversion
 from bact_twin_architecture.interfaces.translator_service import TranslatorServiceBase
-from bact_twin_architecture.utils.unit_conversion import LinearUnitConversion, EnergyIndependentLinearUnitConversion
-from bact_twin_bessyii_impl.bl.bessyii_nomen_clature import name_matches_horizontal_steerer_name, \
-    name_matches_vertical_steerer_name, name_matches_steerer_name
+from bact_twin_architecture.utils.unit_conversion import (
+    LinearUnitConversion,
+    EnergyIndependentLinearUnitConversion,
+)
+from bact_twin_bessyii_impl.bl.bessyii_nomen_clature import (
+    name_matches_horizontal_steerer_name,
+    name_matches_vertical_steerer_name,
+    name_matches_steerer_name,
+    name_matches_quadrupole_name,
+    name_matches_sextupole_name,
+    YellowPages,
+    bessyii_yellow_pages,
+)
 
 from ..data.querries import get_magnets
-from ..data.constants import ring_parameters
+from ..data.constants import ring_parameters, cavity_names
 from ...core.model.elementmodel import MagnetElementSetup
 
 logger = logging.getLogger("dt4acc")
@@ -32,7 +46,9 @@ class LiaisonManager(LiaisonManagerBase):
         try:
             return self.inverse_lut[id_]
         except KeyError as ke:
-            logger.error(f"{self.__class__.__name__} I did not find id {id_} in lookup table: {ke}")
+            logger.error(
+                f"{self.__class__.__name__} I did not find id {id_} in lookup table: {ke}"
+            )
 
 
 class TranslatorService(TranslatorServiceBase):
@@ -43,8 +59,32 @@ class TranslatorService(TranslatorServiceBase):
         try:
             return self.lut[id_]
         except KeyError as ke:
-            logger.error(f"{self.__class__.__name__}: I did not find id {id_} in lookup table: {ke}")
+            logger.error(
+                f"{self.__class__.__name__}: I did not find id {id_} in lookup table: {ke}"
+            )
+            od = self.objects_for_device(id_)
+            logger.warning(f"{self.__class__.__name__}: For the device I know {od}")
+            em = self.objects_for_lat_elem(id_)
+            logger.warning(
+                f"{self.__class__.__name__}: For the lattice element I know {em}"
+            )
             raise ke
+
+    def objects_for_lat_elem(self, id_: ConversionID):
+        return {
+            key: to
+            for key, to in self.lut.items()
+            if id_.lattice_property_id.element_name
+            == key.lattice_property_id.element_name
+        }
+
+    def objects_for_device(self, id_: ConversionID):
+        return {
+            key: to
+            for key, to in self.lut.items()
+            if id_.device_property_id.device_name == key.device_property_id.device_name
+        }
+
 
 def remove_id(d: Dict) -> Dict:
     nd = d.copy()
@@ -57,8 +97,39 @@ def magnet_infos_from_db() -> Sequence[MagnetElementSetup]:
     return [MagnetElementSetup(**remove_id(info)) for info in get_magnets().to_list()]
 
 
-def build_managers() -> (LiaisonManagerBase, TranslatorServiceBase):
-    """A first poor mans implementation of the database
+def element_method(element_name):
+    if name_matches_horizontal_steerer_name(element_name):
+        return "x_kick"
+    elif name_matches_vertical_steerer_name(element_name):
+        return "y_kick"
+    elif name_matches_quadrupole_name(element_name):
+        return "K"
+    elif name_matches_sextupole_name(element_name):
+        return "H"
+    else:
+        raise AssertionError(f"Don't know how to handle {element_name}")
+
+
+def extract_host_element_name(element_name: str) -> str:
+    if name_matches_steerer_name(element_name):
+        return element_name[1:]
+    return element_name
+
+
+def construct_energy_independent_linear_conversion(
+    slope: float,
+) -> EnergyIndependentLinearUnitConversion:
+    if slope is None:
+        raise AssertionError("Refusing creating linear unit conversion without slope")
+    return EnergyIndependentLinearUnitConversion(
+        slope=1.0 / slope, intercept=0.0, brho=ring_parameters.brho
+    )
+
+
+def build_managers(
+    yp: YellowPages = bessyii_yellow_pages(),
+) -> (LiaisonManagerBase, TranslatorServiceBase):
+    """A first poor mans implementation of liasion manager and Translation service for BessyII
 
     Todo:
         Which info is already in database and better obtained from database?
@@ -69,47 +140,90 @@ def build_managers() -> (LiaisonManagerBase, TranslatorServiceBase):
     # Make sure that names are unique ... everything down the list depends on it
     magnet_names = set([info.name for info in infos])
     if len(list(magnet_names)) != len(infos):
-        raise AssertionError("Magnet names seem not to be unique, but is assumption of all further processing")
+        raise AssertionError(
+            "Magnet names seem not to be unique, but is assumption of all further processing"
+        )
 
     power_converter_names = set([info.pc for info in infos])
     power_converter_feeds = {
-        pc_name: [info.name for info in infos if info.pc == pc_name] for pc_name in power_converter_names
+        pc_name: [info.name for info in infos if info.pc == pc_name]
+        for pc_name in power_converter_names
     }
 
     magnet_lut = {info.name: info for info in infos}
     # todo: check if property must be different for the different magnets ...
 
-
-
     # first for steerers : for AT these are angles applied to the host magnet
     # I use that I know one pc goes to one steerer
     inverse_lut = {
-        DevicePropertyID(device_name=info.pc, property="set_current"):
-            (LatticeElementPropertyID(element_name=info.name[1:], property="x_kick"),)
-        for info in infos if name_matches_horizontal_steerer_name(info.name)
-
+        DevicePropertyID(device_name=info.pc, property="set_current"): (
+            LatticeElementPropertyID(element_name=info.name[1:], property="x_kick"),
+        )
+        for info in infos
+        if info.name in yp.horizontal_steerer_names()
     }
-    inverse_lut.update({
-        DevicePropertyID(device_name=info.pc, property="set_current"):
-            (LatticeElementPropertyID(element_name=info.name[1:], property="y_kick"),)
-        for info in infos if name_matches_vertical_steerer_name(info.name)
-
-    })
+    inverse_lut.update(
+        {
+            DevicePropertyID(device_name=info.pc, property="set_current"): (
+                LatticeElementPropertyID(element_name=info.name[1:], property="y_kick"),
+            )
+            for info in infos
+            if info.name in yp.vertical_steerer_names()
+        }
+    )
 
     # Test that steerer power converters only feed one before going to the next step
     for key in inverse_lut:
         corr_pc = key.device_name
         magnet_names = power_converter_feeds[corr_pc]
         if len(magnet_names) != 1:
-            raise AssertionError(f"Found {magnet_names} magnets on assumed corrector power supply {corr_pc}")
+            raise AssertionError(
+                f"Found {magnet_names} magnets on assumed corrector power supply {corr_pc}"
+            )
 
     # quadrupoles and sextupoles
     steerer_pc_names = [key.device_name for key in inverse_lut]
-    inverse_lut.update({
-        DevicePropertyID(device_name=pc_name, property="set_current"):
-            tuple([LatticeElementPropertyID(element_name=magnet_name, property="K") for magnet_name in magnet_names])
-        for pc_name, magnet_names in power_converter_feeds.items() if pc_name not in steerer_pc_names
-    })
+    inverse_lut.update(
+        {
+            DevicePropertyID(device_name=pc_name, property="set_current"): tuple(
+                [
+                    LatticeElementPropertyID(element_name=magnet_name, property="K")
+                    for magnet_name in magnet_names
+                    if magnet_name in yp.quadrupole_names()
+                ]
+            )
+            for pc_name, magnet_names in power_converter_feeds.items()
+            if pc_name not in steerer_pc_names
+        }
+    )
+    inverse_lut.update(
+        {
+            DevicePropertyID(device_name=pc_name, property="set_current"): tuple(
+                [
+                    LatticeElementPropertyID(element_name=magnet_name, property="H")
+                    for magnet_name in magnet_names
+                    if magnet_name in yp.sextupole_names()
+                ]
+            )
+            for pc_name, magnet_names in power_converter_feeds.items()
+            if pc_name not in steerer_pc_names
+        }
+    )
+
+    inverse_lut.update(
+        {
+            DevicePropertyID(device_name=pc_name, property="set_current"): tuple(
+                [
+                    LatticeElementPropertyID(
+                        element_name=magnet_name, property="main_strength"
+                    )
+                    for magnet_name in magnet_names
+                ]
+            )
+            for pc_name, magnet_names in power_converter_feeds.items()
+            if pc_name not in steerer_pc_names
+        }
+    )
 
     # Add lut for quadrupoles and sextupole
     # Furthermore to feed through the K value ...
@@ -117,68 +231,160 @@ def build_managers() -> (LiaisonManagerBase, TranslatorServiceBase):
     #     is that appropriate ?
     #     Should one rather use a handler for lattice elements
     quad_updates = dict()
-    for axis_name  in "x", "y", "K":
-        quad_updates.update({
-            DevicePropertyID(device_name=info.name, property=axis_name):
-                (LatticeElementPropertyID(element_name=info.name, property=axis_name),)
-            for info in infos if info.type in ["Sextupole", "Quadrupole"]
-        })
+    for axis_name in "x", "y", "K":
+        quad_updates.update(
+            {
+                DevicePropertyID(device_name=info.name, property=axis_name): (
+                    LatticeElementPropertyID(
+                        element_name=info.name, property=axis_name
+                    ),
+                )
+                for info in infos
+                if info.type in ["Sextupole", "Quadrupole"]
+            }
+        )
     inverse_lut.update(quad_updates)
 
+    # Cavities and master clock
+    inverse_lut.update(
+        {
+            DevicePropertyID(device_name=name, property="frequency"): (
+                LatticeElementPropertyID(element_name=name, property="frequency"),
+            )
+            for name in cavity_names
+        }
+    )
+    inverse_lut.update(
+        {
+            DevicePropertyID(
+                device_name="master_clock", property="reference_frequency"
+            ): tuple(
+                [
+                    LatticeElementPropertyID(element_name=name, property="frequency")
+                    for name in cavity_names
+                ]
+            )
+        }
+    )
 
     forward_lut = None
     lm = LiaisonManager(forward_lut=forward_lut, inverse_lut=inverse_lut)
-
-    def element_method(element_name):
-        device_name = "HS4M2D1R"
-        if element_name == device_name:
-            pass
-        if name_matches_horizontal_steerer_name(element_name):
-            return "x_kick"
-        elif name_matches_vertical_steerer_name(element_name):
-            return "y_kick"
-        else:
-            return "K"
-
-    def extract_host_element_name(element_name: str) -> str:
-        if name_matches_steerer_name(element_name):
-            return element_name[1:]
-        return element_name
-
-    def construct_energy_independent_linear_conversion(slope: float) -> EnergyIndependentLinearUnitConversion:
-        if slope is None:
-            raise AssertionError("Refusing creating linear unit conversion without slope")
-        return EnergyIndependentLinearUnitConversion(slope=1.0/slope, intercept=0.0, brho=ring_parameters.brho)
 
     # start to build it for the magnets ... power converter feed
     translator_lut = {
         ConversionID(
             LatticeElementPropertyID(
                 element_name=extract_host_element_name(info.name),
-                property=element_method(info.name)
+                property=element_method(info.name),
             ),
-            DevicePropertyID(device_name=info.pc, property="set_current")
+            DevicePropertyID(device_name=info.pc, property="set_current"),
         ):
-            # todo: check for the correct conversion
-            construct_energy_independent_linear_conversion(slope=info.magnetic_strength)
+        # todo: check for the correct conversion
+        construct_energy_independent_linear_conversion(slope=info.magnetic_strength)
         for info in infos
     }
+    # add look up for value using main strength for quadrupoles and sextupoles
+    translator_lut.update(
+        {
+            ConversionID(
+                LatticeElementPropertyID(
+                    element_name=extract_host_element_name(info.name),
+                    property="main_strength",
+                ),
+                DevicePropertyID(device_name=info.pc, property="set_current"),
+            ):
+            # todo: check for the correct conversion
+            construct_energy_independent_linear_conversion(slope=info.magnetic_strength)
+            for info in infos
+            if info.type in ["Sextupole", "Quadrupole"]
+        }
+    )
 
     axis_updates = dict()
-    for axis_name  in "x", "y", "K":
-        axis_updates.update({
-            ConversionID(
-                lattice_property_id=LatticeElementPropertyID(element_name=info.name, property=axis_name),
-                device_property_id=DevicePropertyID(device_name=info.name, property=axis_name)
-            )
-            : LinearUnitConversion(slope=1.0, intercept=0.0)
-            for info in infos if info.type in ["Sextupole", "Quadrupole"]
-        })
-    translator_lut.update(axis_updates)
     # start to build it for quadrupoles and sextupoles axes
+    for axis_name in "x", "y":
+        axis_updates.update(
+            {
+                ConversionID(
+                    lattice_property_id=LatticeElementPropertyID(
+                        element_name=info.name, property=axis_name
+                    ),
+                    device_property_id=DevicePropertyID(
+                        device_name=info.name, property=axis_name
+                    ),
+                ): LinearUnitConversion(slope=1.0, intercept=0.0)
+                for info in infos
+                if info.type in ["Sextupole", "Quadrupole"]
+            }
+        )
+    translator_lut.update(axis_updates)
+
+    # K and H as requested from the device world ... should it be there?
+    translator_lut.update(
+        {
+            ConversionID(
+                lattice_property_id=LatticeElementPropertyID(
+                    element_name=info.name, property="K"
+                ),
+                device_property_id=DevicePropertyID(
+                    device_name=info.name, property="K"
+                ),
+            ): LinearUnitConversion(slope=1.0, intercept=0.0)
+            for info in infos
+            if info.type in ["Quadrupole"]
+        }
+    )
+    # Todo: questionable if that should be handled here ...
+    # view should know what to delegate to the lattice
+    translator_lut.update(
+        {
+            ConversionID(
+                lattice_property_id=LatticeElementPropertyID(
+                    element_name=info.name, property="H"
+                ),
+                device_property_id=DevicePropertyID(
+                    device_name=info.name, property="H"
+                ),
+            ): LinearUnitConversion(slope=1.0, intercept=0.0)
+            for info in infos
+            if info.type in ["Sextupole"]
+        }
+    )
+
+    # cavities
+    translator_lut.update(
+        {
+            ConversionID(
+                lattice_property_id=LatticeElementPropertyID(
+                    element_name=name, property="frequency"
+                ),
+                device_property_id=DevicePropertyID(
+                    device_name=name, property="frequency"
+                ),
+            ): LinearUnitConversion(
+                slope=1e-3, intercept=0.0
+            )  # BESSY II uses kHz for the cavities clock
+            for name in cavity_names
+        }
+    )
+
+    translator_lut.update(
+        {
+            ConversionID(
+                LatticeElementPropertyID(element_name=name, property="frequency"),
+                DevicePropertyID(
+                    device_name="master_clock", property="reference_frequency"
+                ),
+            ): LinearUnitConversion(
+                slope=1e-3, intercept=0.0
+            )  # BESSY II uses kHz for the master clock
+            for name in cavity_names
+        }
+    )
 
     tm = TranslatorService(translator_lut)
     return lm, tm
+
 
 if __name__ == "__main__":
     build_managers()
