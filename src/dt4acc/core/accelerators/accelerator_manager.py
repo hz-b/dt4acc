b72af559d7c3508b916ec26d5b2354e5d7171e0f
Waheed <wsulimankhail@gmail.com>
2025-07-14 16:29:11 +0200

    [TASK] mongo dependency temporaryly removed

--- src/dt4acc/core/accelerators/accelerator_manager.py
+++ src/dt4acc/core/accelerators/accelerator_manager.py
@@ -46,7 +46,7 @@ class AcceleratorManager:
         """
         try:
             from lat2db.model.accelerator import Accelerator
-            acc_model = Accelerator()
+            acc_model = Accelerator(file_name ="bessyii_lattice_json.json", from_json= True)
 
             # Initialize the accelerator with required components
             self.accelerator = AcceleratorImpl(
