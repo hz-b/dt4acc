911888c3366e52f1e63c66c6791715bed08db1f5
Waheed <wsulimankhail@gmail.com>
2025-07-15 11:33:05 +0200

    [TASK] changed some of the log printings, added a default movement to a corrector temporarily so twiss is called with real values

--- src/dt4acc/custom_epics/ioc/tasks.py
+++ src/dt4acc/custom_epics/ioc/tasks.py
@@ -1,9 +1,11 @@
 import asyncio
 
+from p4p.client.asyncio import Context
+
 from ...core.views.shared_view import get_view_instance
 from ...core.utils.logger import get_logger
 
-
+ctx = Context("pva")
 logger = get_logger()
 view = get_view_instance()
 heartbeat_task = None  # Global variable to store the heartbeat task reference
@@ -18,6 +20,9 @@ async def monitor_heartbeat():
     global heartbeat_task
     while True:
         if heartbeat_task is None or heartbeat_task.done():
+            # todo hardcode the setpoint to a value, so real calculation runs. this needs to be addressed differently
+            await ctx.put('Anonym:VS3P2T5R:set', 1e-3)
+            await ctx.put('Anonym:VS3P2T5R:set', 0e-3)
             logger.warning("Heartbeat loop initial start or it was terminated unexpectedly. Restarting...")
             heartbeat_task = asyncio.create_task(heartbeat_loop())
         await asyncio.sleep(1)
