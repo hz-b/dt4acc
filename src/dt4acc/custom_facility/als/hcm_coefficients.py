d3be778f4bcebb38807a6bb5b593f55489e98998
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-05-21 11:00:20 +0200

    [TASK] liasion translator yp setup

--- src/dt4acc/custom_facility/als/hcm_coefficients.py
+++ src/dt4acc/custom_facility/als/hcm_coefficients.py
@@ -1,1 +1,23 @@
+from typing import Sequence
 
+
+def hcm_coefficients(device_index: Sequence[int]) -> Sequence[float]:
+    device_index = tuple(device_index)
+    if device_index[1] in (2, 7):
+        return 0.88 * 4.070e-4, 0.5
+    elif device_index[1] in (1, 8):
+        return 0.95 * 4.070e-4, 0.5
+    elif device_index in {(5, 3), (5, 6)}:
+        return 0.6329 * 9.500e-4 * 0.45, 0.2030
+    elif device_index in {(5, 5), (7, 4)}:
+        return 0.5046 * 10.250e-4 * 0.45, 0.2030
+    elif device_index[1] in (3, 6):
+        return 0.7667 * 9.500e-4, 0.2030
+    elif device_index[1] in (4, 5):
+        return 0.6156 * 10.250e-4, 0.2030
+    elif device_index[1] == 10:
+        return 1.0753 * 0.2e-4, 0.2030
+    else:
+        raise ValueError(f"Unknown device {device_index} (checking only {device_index[1]}")
+
+    assert 0, "Should not reach here"
---
+++ src/dt4acc/custom_facility/als/liaison_translator_setup.py
@@ -0,0 +1,302 @@
+import itertools
+import os
+import pprint
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict, Tuple, Sequence, List
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+from scipy.io import loadmat
+
+from bact_mml_json_importer.data_model.mml_ao import FamilyInfoCollection
+from dt4acc.custom_facility.als.hcm_coefficients import hcm_coefficients
+from dt4acc.custom_facility.als.model import MMLStyleDeviceIdentifier
+from dt4acc.custom_facility.als.read_lattice import als_load_lattice, default_filename
+from dt4acc.custom_facility.als.readin_ao import als_ring_ao_data
+from dt4acc.custom_facility.als.vcm_coefficients import vcm_coefficients
+from dt4acc_lib.bl.liaison_manager import LiaisonManager
+from dt4acc_lib.bl.translator_service import TranslatorService
+from dt4acc_lib.interfaces.utils.liaison_manager import LiaisonManagerBase
+from dt4acc_lib.interfaces.utils.yellow_pages import YellowPagesBase
+from dt4acc_lib.model.utils.identifiers import DevicePropertyID, LatticeElementPropertyID, ConversionID
+from dt4acc_lib.model.utils.liaison_manager_lookup_table import LiaisonManagerInverseLookupTable, \
+    LiaisonManagerForwardLookupTable, LiaisonManagerForwardLookupElement, LiaisonManagerInverseLookupElement
+from dt4acc_lib.model.utils.translator_manager_lookup_table import TranslatorLookupTable, TranslatorLookupTableElement, \
+    PolynomCoefficients
+
+
+def uuids_of_at_elements(
+    elements: Sequence, family_name: str, indices: Sequence[int], alternative_family_names: Sequence[str] = []
+) -> Sequence[str]:
+    """Returns uuids of the lattice elments whose index is found at ATIndex
+
+    Checks that the stored AT element index points to an AT element with expected family name
+
+    Todo:
+        separate responsibility
+
+        build the names here
+        check that its points to the correct place in the lattice
+        at a second step
+
+    """
+    indices = np.asarray(indices)
+    assert np.all(indices > 0)
+    indices = indices - 1
+    names = [elements[idx].uuid for idx in indices]
+
+    for cnt, tmp  in enumerate(zip(indices, names)):
+        idx, name = tmp
+        if not name.startswith(family_name):
+            for alternate in alternative_family_names:
+                if name.startswith(alternate):
+                    break
+            else:
+                raise ValueError(
+                    f"family {family_name}: element no {cnt} at index {idx} name {name} not cohereent to family name"
+                )
+    return names
+
+
+def extract_family_member_names(lat, ao_table: Dict[str, FamilyInfoCollection], family_name: str, alternative_family_names: Sequence[str] = None) -> Sequence[str]:
+    alternative_family_names = alternative_family_names or []
+    indices = ao_table[family_name].AT.get_element_indices()
+    dev_list = ao_table[family_name].get_device_list()
+
+    assert len(indices) == len(dev_list), f"Check failed for family {family_name} n indices = {len(indices)}, n devs = {len(dev_list)}"
+
+    uuids_of_at_elements(lat, family_name, list(itertools.chain.from_iterable(indices)), alternative_family_names=alternative_family_names)
+    identifiers = [
+        MMLStyleDeviceIdentifier(family=family_name, sector=int(sector), child=int(child)) for sector, child in dev_list
+    ]
+    return identifiers
+
+
+standard_ao_families = dict(
+    focusing_quadrupoles="QF",
+    defocusing_quadrupoles="QD",
+    horizontal_steerers="HCM",
+    vertical_steerers="VCM",
+    quadrupoles="QUAD",
+)
+
+
+def create_yellow_pages_input(ao_table: Dict[str, FamilyInfoCollection], lat) -> Dict[str, Sequence[str]]:
+    """
+
+    Todo:
+        use standard family names?
+
+    """
+    standard_families = ["QF", "QD", "SF", "SHF", "SHD", "QFA", "QDA", "BPM",]
+    names = {
+        fam: extract_family_member_names(lat, ao_table, fam)
+        for fam in standard_families
+    }
+    names["BEND"] = extract_family_member_names(lat, ao_table, "BEND", alternative_family_names=["BS"])
+    names["HCM"] = extract_family_member_names(lat, ao_table, "HCM", alternative_family_names=["COR"])
+    names["VCM"] = extract_family_member_names(lat, ao_table, "VCM", alternative_family_names=["COR"])
+
+    # what are these ?
+    names["BSC"] = extract_family_member_names(lat, ao_table, "BSC", alternative_family_names=["BS"])
+    names["SQSF"] = extract_family_member_names(lat, ao_table, "SQSF", alternative_family_names=["SFF"])
+    names["SQSD"] = extract_family_member_names(lat, ao_table, "SQSD", alternative_family_names=["SDD"])
+
+    names["RF"] = extract_family_member_names(lat, ao_table, "RF", alternative_family_names=["CAV"])
+    defocusing_quads = extract_family_member_names(lat, ao_table, "QD")
+    # dict(quadrupoles=)
+
+    # now build the yellow pages using member of
+    # here one item can belong to more than one family
+    yp_dict = defaultdict(list)
+    for ref_fam_name, family_members in names.items():
+        # These should be rather tags, and always a sequence!
+        members_of = ao_table[ref_fam_name].member_of()
+        for family_name in members_of:
+            yp_dict[str(family_name)].extend(family_members)
+
+    # print("names sorted to reference families")
+    # pprint.pprint(names, compact=True)
+
+    # print("families (rather tags)")
+    # pprint.pprint(yp_dict, compact=True)
+    # print(f"Total number of elements {np.sum([len(v) for v in names.values()])}")
+    # print("Families not exported to yp:", set(tuple(ao_table)).difference(tuple(names)))
+
+    return dict(yp_dict)
+
+
+def get_element_uuids_for_device(ao_table, lat, dev_id: MMLStyleDeviceIdentifier) -> Sequence[str]:
+    sel = ao_table[dev_id.family]
+    device_index = sel.get_device_list().index(dev_id.mml_device_index())
+    lattice_element_indices = np.asarray(sel.AT.get_element_indices()[device_index])
+    assert (lattice_element_indices > 1).all()
+    uuid = [elem.uuid for elem in lat[lattice_element_indices - 1]]
+    return uuid
+
+
+def create_liaison_lut(yp: YellowPagesBase, ao: Dict[str, FamilyInfoCollection], lat) -> Tuple[LiaisonManagerForwardLookupTable, LiaisonManagerInverseLookupTable]:
+    forward_lut = []
+    inverse_lut = []
+
+    for family_name, property in [
+        # just to get started
+        ("QUAD", "main_strength"), ("SEXT", "main_strength"),
+        ("HCM", "x_kick"), ("VCM", "y_kick")
+    ]:
+        for corr in yp.get(family_name):
+            dev_prop = DevicePropertyID(device_name=corr, property="set_current")
+            element_names = get_element_uuids_for_device(ao_table=ao, lat=lat, dev_id=corr)
+            elem_props = [LatticeElementPropertyID(element_name=name, property=property) for name in element_names]
+
+            for elm_prop in elem_props:
+                forward_lut.append(LiaisonManagerForwardLookupElement(lat_id=elm_prop, dev_ids=[dev_prop]))
+            inverse_lut.append(LiaisonManagerInverseLookupElement(dev_id=dev_prop, lat_ids=elem_props))
+
+    fwd = LiaisonManagerForwardLookupTable(forward_lut)
+    bwd = LiaisonManagerInverseLookupTable(inverse_lut)
+    fwd.verify()
+    bwd.verify()
+    return fwd, bwd
+
+
+def create_translator_luts(yp: YellowPagesBase, lm: LiaisonManagerBase, ao: Dict[str, FamilyInfoCollection], lat) -> TranslatorLookupTable:
+    translator_lut: List[TranslatorLookupTableElement] = []
+
+    for dev_name in yp.get("HCM"):
+        d = lm.objects_for_device(dev_name=dev_name)
+        src, = d
+        assert src.device_name == dev_name
+        tgt, = d.values()
+        coeffs = hcm_coefficients(src.device_name.mml_device_index())
+        # Need to check if that is the correct coefficent
+        # For now I assume it returns a scale factor for k and B
+        translator_lut.append(
+            TranslatorLookupTableElement(
+                conversion_id=ConversionID(tgt,src),
+                conversion_info=PolynomCoefficients(coeffs=[0.0, coeffs[0]], energy_dependent=True),
+            )
+        )
+
+    for dev_name in yp.get("VCM"):
+        d = lm.objects_for_device(dev_name=dev_name)
+        src, = d
+        assert src.device_name == dev_name
+        tgt, = d.values()
+
+        coeffs = vcm_coefficients(src.device_name.mml_device_index())
+        # Need to check if that is the correct coefficent
+        translator_lut.append(
+            TranslatorLookupTableElement(
+                conversion_id=ConversionID(tgt,src),
+                conversion_info=PolynomCoefficients(coeffs=[0.0, coeffs[0]], energy_dependent=True),
+            )
+        )
+
+    for dev_name in yp.get("QUAD"):
+        d = lm.objects_for_device(dev_name=dev_name)
+        src, = d
+        assert src.device_name == dev_name
+        tgt, = d.values()
+
+    for dev_name in yp.get("SEXT"):
+        d = lm.objects_for_device(dev_name=dev_name)
+        src, = d
+        assert src.device_name == dev_name
+        tgt, = d.values()
+
+    r = TranslatorLookupTable(lut=translator_lut)
+    return r
+
+
+    dev_names = yp.get("HCM")
+    pass
+    # All magnets — primary conversion keyed by (magnet, PC)
+
+
+def to_single_vector(data) -> Sequence[float]:
+    t_data = data
+    # Go down as long as the contained element
+    # still contains only one element
+    while True:
+        try:
+            l =  len(t_data)
+        except TypeError:
+            return t_data
+
+        if len(t_data) == 1:
+            t_data, = t_data
+        else:
+            break
+        dtype = t_data.dtype
+        pass
+    dtype = t_data.dtype
+    return t_data
+
+def unpack_ramp_data(data):
+    while True:
+        data = to_single_vector(data)
+        dtype = data.dtype
+        if isinstance(dtype, np.dtypes.VoidDType):
+            d = {
+                dtype_name: unpack_ramp_data(data[dtype_name])
+                for dtype_name in data.dtype.fields.keys()
+            }
+            return d
+            pass
+        else:
+            break
+    return data
+
+def numrec_array_to_dict(data):
+    return
+
+
+def load_ramp_data():
+    path = Path(os.environ["HOME"]) / "Devel/github/matlab-middle-layer/machine/ALS//StorageRingOpsData/"
+    filename = path / "Model/alsrampup.mat"
+    filename = path / "Greg/alsrampup.mat"
+    data = loadmat(filename)
+    ramp_data = data["RampTable"]
+
+    d = {
+        dtype_name: unpack_ramp_data(ramp_data[dtype_name])
+        for dtype_name in ramp_data.dtype.fields.keys()
+    }
+    gev_as_coor = d.pop("GeV")
+    d.pop("UpperLattice")
+    d.pop("LowerLattice")
+
+    d2 = {
+        k: xr.DataArray(data=v["Setpoint"], dims="GeV", coords=[gev_as_coor])
+        for k, v in d.items()
+    }
+    r = xr.Dataset(d2)
+    return r
+
+    df = pd.DataFrame(d)
+    return data
+
+
+def main():
+    ramp_data = load_ramp_data()
+    pass
+
+    lat = als_load_lattice(default_filename)
+    ao_model = als_ring_ao_data()
+
+    yp = create_yellow_pages_input(ao_model, lat)
+    yp
+
+    fwd_lut, inv_lut = create_liaison_lut(yp, ao_model, lat)
+    lm = LiaisonManager(forward_lut=fwd_lut, inverse_lut=inv_lut)
+    ts_lut = create_translator_luts(yp, lm, ao_model, lat)
+    ts = TranslatorService(lut=ts_lut, brho=None)
+    ts
+
+
+
+if __name__ == "__main__":
+    main()
