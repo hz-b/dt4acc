e310ba651d62d122cfdf17dd734560b30d0c42ab
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-05-22 18:23:38 +0200

    [TASK] initialising "monitor" data, only read them from backend if not marked as delayed

--- src/dt4acc/custom_facility/als/controller.py
+++ src/dt4acc/custom_facility/als/controller.py
@@ -9,6 +9,7 @@ from softioc.pythonSoftIoc import RecordWrapper
 from dt4acc.core.interfaces.controller_interface import ControllerInterface
 from dt4acc.custom_epics.ioc.controller import Controller as EpicsController, dispatcher
 from dt4acc.custom_epics.ioc.pv_setup import (
+    initialize_master_clock_pvs,
     initialize_orbit_pvs,
     initialize_twiss_pvs,
     initialize_tune_pvs,
@@ -60,9 +61,12 @@ async def build_ao_record(
 
 
 async def build_ai_record(builder, model: Monitor, controller: ControllerInterface):
-    initial_val = unpack_translated_reading_expecting_single_float(
-        await controller.trigger_read([model.rcmd])
-    )
+    if model.update == "delayed":
+        initial_val = math.nan
+    else:
+        initial_val = unpack_translated_reading_expecting_single_float(
+            await controller.trigger_read([model.rcmd])
+        )
     rec = builder.aIn(
         model.pv_name,
         initial_value=initial_val,
@@ -114,6 +118,7 @@ class ALSEpicsController(EpicsController):
 
         d = {
             **recs,
+            **await initialize_master_clock_pvs(self.builder, self.delegate),
             **initialize_orbit_pvs(self.builder),
             **initialize_twiss_pvs(self.builder),
             **initialize_tune_pvs(self.builder),
