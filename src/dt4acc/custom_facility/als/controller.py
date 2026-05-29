45dc95e8c5b07c5ba3d1d20659d335883869fb58
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-05-29 13:59:38 +0200

    [Snapshot] after initalisation: reread all variables

--- src/dt4acc/custom_facility/als/controller.py
+++ src/dt4acc/custom_facility/als/controller.py
@@ -77,7 +77,6 @@ async def build_ao_record(
     # don't forget the ones that should be updated
     # when this changes: e.g. read backs from power converters
     reads = reads + model.reads
-    pass
 
     async def update(val: float):
         return await controller.update(
@@ -123,7 +122,7 @@ async def initialize_pvs_from_model(
     models: Sequence[Union[Setpoint, Monitor]],
     controller: ControllerInterface,
 ) -> Dict[ReadCommand, RecordWrapper]:
-    """initalise pvs based on their Setpoint or Monitor model
+    """initialise pvs based on their Setpoint or Monitor model
 
     Ignore pvs that can not be instantiated
 
@@ -185,6 +184,27 @@ class ALSEpicsController(EpicsController):
 
         self.builder.LoadDatabase()
         softioc.iocInit(dispatcher)
+
+        # collect once all readings form all views
+        # trigger update and then be finished
+        # Read all in once at start up
+        reads = list(self.delegate.view.process_variables)
+        async def read_one_by_one(rcmd):
+            # so that we can log the ones that fail
+            try:
+                r = await self.trigger_read([rcmd])
+            except Exception as ex:
+                logger.error(f"{self.__class__.__name__} {self.name} failed to retrieve data for {rcmd}")
+                # can be still useful to report in one batch
+                return rcmd, None
+            return rcmd, r
+
+        read_result = [await read_one_by_one(rcmd) for rcmd in reads]
+        read_result = [(rcmd, translated) for rcmd, translated in read_result if translated is not None]
+        # Need to combine translated...
+        for rcmd, translated in read_result:
+            for data in translated.data:
+                await self.delegate.view.dispatch(rcmd, data)
 
 
 __all__ = ["ALSEpicsController"]
