5dce132a0a04e92cb4491926fe22556006519fa0
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-06-01 14:24:18 +0200

    [TASK] use at.read_json instead of lat2db
    A dependency to remove

--- src/dt4acc/custom_facility/als/read_lattice.py
+++ src/dt4acc/custom_facility/als/read_lattice.py
@@ -61,13 +61,31 @@ default_filename = (
     / "als_thering_tst.json"
 )
 
-
+test_filename = (
+    Path(os.environ.get("HOME"))
+    / "Documents"
+    / "dt4acc_als_data"
+    / "als_thering_at_compat_tst.json"
+)
 def als_load_lattice(filename: str, energy: float = default_energy):
     with open(filename, "rt") as fp:
         sequence_data = json.load(fp)
 
-    seq = als_add_uuid_to_lattice_elements(factory(sequence_data, energy))
-    r = at.Lattice(seq, name="ALS storage ring", energy=energy)
+    with open(test_filename, "wt") as fp:
+        json.dump(
+            dict(
+                atjson=1,
+                elements=sequence_data,
+                energy=default_energy
+            ),
+            fp
+        )
+        # sequence_data =
+
+    r = at.load_json(test_filename, from_at=True, energy=default_energy)
+    als_add_uuid_to_lattice_elements(r)
+    # seq = als_add_uuid_to_lattice_elements(factory(sequence_data, energy))
+    # r = at.Lattice(seq, name="ALS storage ring", energy=energy)
     r.enable_6d()
     r.cavpts = "CAV*"
     r.set_cavity_phase(cavpts=r.cavpts)
