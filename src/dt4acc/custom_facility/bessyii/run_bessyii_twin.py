75845ccf3beb30e063a9621bcb1b1605288e6954
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-06-09 17:55:12 +0200

    [TASK] start BESSY II twin with solely standard controller

--- src/dt4acc/custom_facility/bessyii/run_bessyii_twin.py
+++ src/dt4acc/custom_facility/bessyii/run_bessyii_twin.py
@@ -1,28 +1,53 @@
+import asyncio
+import functools
 import getpass
 import json
 import logging
 import os
+from typing import Dict, Any
+
 import at
 from importlib import resources
 
 from softioc import builder, softioc
 
+from dt4acc.core.bl.controller import Controller, read_and_dispatch
+from dt4acc.core.interfaces.controller_interface import ControllerInterface
+from dt4acc.custom_epics.ioc.pv_setup import (
+    initialize_master_clock_pvs,
+    initialize_cavity_pvs,
+    initialize_power_converter_pvs,
+    initialize_machine_info_pvs,
+    initialize_survey_info_pvs,
+    initialize_orbit_object_pvs,
+    initialize_orbit_pvs,
+    initialize_twiss_pvs,
+    initialize_tune_pvs,
+    initialize_other_pvs,
+)
 from dt4acc.core.bl.controller import Controller
+from dt4acc_lib.pyat_simulator.accelerator_simulator import PyATAcceleratorSimulator
 from dt4acc_lib.pyat_simulator.proxies.proxy_factory import ElementProxyFactory
 from dt4acc_lib.pyat_simulator.simulator_backend import SimulatorBackend
 from dt4acc_lib.bl.command_rewritter import CommandRewriter
 from dt4acc_lib.model.utils.command import ReadCommand
-from dt4acc.core.bl.translating_command_execution_engine import TranslatingCommandExecutionEngine
+from dt4acc.core.bl.translating_command_execution_engine import (
+    TranslatingCommandExecutionEngine,
+)
 from dt4acc.custom_epics.ioc.orbit_pva import OrbitTwinServer
-from dt4acc.custom_epics.ioc.controller import Controller as EpicsController, dispatcher
+from dt4acc.custom_epics.ioc.controller import dispatcher
 from dt4acc.custom_epics.ioc.view import View
 from dt4acc.custom_facility.bessyii.liasion_translator_setup import load_managers
-from dt4acc.custom_facility.bessyii.pyat_accelerator_simulator import BESSYIIPyAtAcceleratorSimulator
-
+from dt4acc_lib.pyat_simulator.accelerator_simulator_proxy_factory import (
+    PyATAcceleratorSimulator,
+)
 
 logging.basicConfig(level=logging.WARNING)
 
-def main():
+logger = logging.getLogger("dt4acc-bessyii")
+
+
+async def main():
     """Handle all startups
 
     * load liasion manager and translation service
@@ -42,17 +67,14 @@ def main():
         "custom_facility/bessyii/resources/storage_ring/input/bessy2_storage_ring_reflat.json"
     )
     acc = bessyii_pyat_lattice(filename=filename)
-    backend=SimulatorBackend(
+    backend = SimulatorBackend(
         name="BESSYII_on_PyAT",
-        acc=BESSYIIPyAtAcceleratorSimulator(at_lattice=acc, proxy_factory=ElementProxyFactory()),
+        acc=PyATAcceleratorSimulator(at_lattice=acc),
     )
 
     _, lm, ts = load_managers()
 
-    command_rewriter=CommandRewriter(
-        liaison_manager=lm,
-        translation_service=ts
-    )
+    command_rewriter = CommandRewriter(liaison_manager=lm, translation_service=ts)
 
     # Todo: review if a dedicated execution engine
     #       View gets an engine to execute
@@ -63,30 +85,59 @@ def main():
         expected_view_for_output="device",
         num_readings=1,
     )
+
     prefix = os.environ.get("DT4ACC_PREFIX", getpass.getuser())
-    orbit_server = OrbitTwinServer(prefix + ":ORBITCC:rdBpm")
+    orbit_server = OrbitTwinServer(
+        prefix + "ORBITCC:rdBpm",
+        prefix + "ORBITCC:rdModel",
+    )
+
     orbit_server.start()
     view = View(orbit_server=orbit_server)
-    common_controller = Controller(
+
+    controller = Controller(
         name="epics-delegate-ctrller",
         mexec=mexec,
         view=view,
         default_delayed_reads=[
             ReadCommand("track", "pos"),
             ReadCommand("twiss", "parameters"),
-            ReadCommand("tune", "x"),
-            ReadCommand("tune", "y"),
-        ]
+        ],
     )
-    controller = EpicsController(
-        name = "epics_controller",
-        controller_delegate=common_controller,
-        builder=builder,
+    controller.start()
+    if prefix:
+        builder.SetDeviceName(prefix)
+
+    view.update_process_variables(
+        await initialise_pvs(builder=builder, controller=controller)
     )
-    dispatcher(controller.startup)
-    common_controller.start()
+    # Extra reads at startup
+    await read_and_dispatch(
+        controller=controller,
+        view=view,
+        rcmds=[ReadCommand("survey", "s")] + list(controller.default_delayed_reads),
+    )
+    builder.LoadDatabase()
+    softioc.iocInit(dispatcher)
     softioc.interactive_ioc(globals())
 
+
+async def initialise_pvs(
+    builder, controller: ControllerInterface
+) -> Dict[ReadCommand, Any]:
+
+    return {
+        **await initialize_master_clock_pvs(builder, controller=controller),
+        **await initialize_cavity_pvs(builder, controller=controller),
+        **await initialize_power_converter_pvs(builder, controller=controller),
+        **initialize_machine_info_pvs(builder, n_ref_buckets=400),
+        **initialize_survey_info_pvs(builder),
+        **initialize_orbit_object_pvs(builder),
+        **initialize_orbit_pvs(builder),
+        **initialize_twiss_pvs(builder),
+        **initialize_tune_pvs(builder),
+        **initialize_other_pvs(builder),
+    }
 
 
 def bessyii_pyat_lattice_from_dics(seq, energy: float = 1.7185e9):
@@ -107,4 +158,4 @@ def bessyii_pyat_lattice(filename: str, energy: float = 1.7185e9) -> at.Lattice:
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
