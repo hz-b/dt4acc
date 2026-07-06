50d02b4c641690f252e87b7eb6f583da52459106
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-07-06 15:39:09 +0200

    [TASK] use standard controller for EPICS

--- src/dt4acc/custom_facility/als/run_als_twin.py
+++ src/dt4acc/custom_facility/als/run_als_twin.py
@@ -1,35 +1,43 @@
+import asyncio
+import logging
+# logging.basicConfig(level=logging.WARNING)
+
+
 import getpass
 import os
-
+from typing import Any, Dict, Sequence, Union
 
 from softioc import builder, softioc
 
-from dt4acc.core.bl.controller import Controller
-from dt4acc.core.bl.translating_command_execution_engine import TranslatingCommandExecutionEngine
-from dt4acc.custom_epics.ioc.view import View
-from dt4acc.custom_epics.ioc.controller import Controller as EpicsController, dispatcher
-from dt4acc.custom_facility.als.controller import ALSEpicsController
+from dt4acc.core.bl.controller import Controller, read_and_dispatch
+from dt4acc.core.bl.translating_command_execution_engine import (
+    TranslatingCommandExecutionEngine,
+)
+from dt4acc.core.interfaces.controller_interface import ControllerInterface
+from dt4acc.core.model.view import Monitor, Setpoint
+from dt4acc.custom_epics.ioc.controller import dispatcher
+from dt4acc.custom_epics.ioc.pv_setup import initialize_master_clock_pvs, initialize_machine_info_pvs, \
+    initialize_orbit_pvs, initialize_twiss_pvs, initialize_tune_pvs, initialize_calculation_state_pvs
+from dt4acc.custom_epics.ioc.pv_setup_from_model import initialize_pvs_from_model
 from dt4acc.custom_facility.als.liaison_translator_setup import load_managers
-from dt4acc.custom_facility.als.read_lattice import als_load_lattice, default_filename
+from dt4acc.custom_facility.als.read_lattice import als_get_lattice, default_filename
+from dt4acc.custom_facility.als.view import ALSView
 from dt4acc_lib.model.utils.command import ReadCommand
+from dt4acc_lib.pyat_simulator.accelerator_simulator import PyATAcceleratorSimulator
 from dt4acc_lib.pyat_simulator.simulator_backend import SimulatorBackend
 from dt4acc_lib.bl.command_rewritter import CommandRewriter
-from dt4acc_lib.pyat_simulator.accelerator_simulator_proxy_factory import PyATAcceleratorSimulator
 
 
-def main():
+async def main():
 
-    lat = als_load_lattice(default_filename)
-    backend=SimulatorBackend(
+    lat = als_get_lattice(default_filename)
+    backend = SimulatorBackend(
         name="ALS_on_PyAT",
         acc=PyATAcceleratorSimulator(at_lattice=lat),
     )
 
     _, lm, ts, process_variable_views = load_managers()
-    command_rewriter = CommandRewriter(
-        liaison_manager=lm,
-        translation_service=ts
-    )
+    command_rewriter = CommandRewriter(liaison_manager=lm, translation_service=ts)
 
     mexec = TranslatingCommandExecutionEngine(
         backend=backend,
@@ -40,28 +48,56 @@ def main():
         num_readings=1,
     )
     prefix = os.environ.get("DT4ACC_PREFIX", getpass.getuser())
-    view = View()
-    common_controller = Controller(
-        name="epics-delegate-ctrller",
+    view = ALSView(lm=lm, ts=ts)
+    controller = Controller(
+        name="epics-controller",
         mexec=mexec,
         view=view,
         default_delayed_reads=[
-            # ReadCommand("track", "pos"),
-            # ReadCommand("twiss", "parameters"),
+            ReadCommand("track", "pos"),
+            ReadCommand("twiss", "parameters"),
             # ReadCommand("tune", "x"),
             # ReadCommand("tune", "y"),
-        ]
+        ],
     )
-    controller =  ALSEpicsController(
-        name = "epics_controller",
-        controller_delegate=common_controller,
-        builder=builder,
-        process_variable_views=process_variable_views
+    controller.start()
+    if prefix:
+        builder.SetDeviceName(prefix)
+
+    view.update_process_variables(
+        await initialise_pvs(
+            builder=builder,
+            models=process_variable_views,
+            controller=controller
+        )
     )
-    dispatcher(controller.startup)
-    common_controller.start()
+
+    await read_and_dispatch(
+        controller=controller,
+        view=view,
+        rcmds=list(controller.default_delayed_reads),
+    )
+    builder.LoadDatabase()
+    softioc.iocInit(dispatcher)
     softioc.interactive_ioc(globals())
 
 
+async def initialise_pvs(
+    builder,
+    models: Sequence[Union[Monitor, Setpoint]],
+    controller: ControllerInterface
+) -> Dict[ReadCommand, Any]:
+
+    return {
+        **await initialize_pvs_from_model(builder, models, controller),
+        **await initialize_turn_by_turn_p0(builder, controller),
+        **initialize_machine_info_pvs(builder, n_ref_buckets=328),
+        **initialize_orbit_pvs(builder),
+        **initialize_twiss_pvs(builder),
+        **initialize_tune_pvs(builder),
+        **initialize_calculation_state_pvs(builder, controller),
+    }
+
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
